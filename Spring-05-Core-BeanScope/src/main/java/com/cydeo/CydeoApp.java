b6ab87dd2fedcda71912ed2355476c8dc3c34736
Yasar-Oz <yasarozen111@gmail.com>
2022-02-26 20:59:08 -0500

    Prototype scope

---
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/CydeoApp.java
@@ -0,0 +1,42 @@
+package com.cydeo;
+
+import com.cydeo.config.ProjectConfig;
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+
+    public static void main(String[] args) {
+
+        Comment comment = new Comment();
+        comment.setAuthor("John");
+        comment.setText("lkasds daskdlas dsalkd");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        CommentService cs1 = context.getBean(CommentService.class);
+        CommentService cs2 = context.getBean(CommentService.class);
+
+
+        //before prototype - Singleton
+
+//        System.out.println("cs1 = " + cs1); //cs1 = com.cydeo.service.CommentService@5876a9af
+//
+//        System.out.println("cs2 = " + cs2); ////cs2 = com.cydeo.service.CommentService@5876a9af
+//
+//        System.out.println("cs2==cs1 => " + (cs2==cs1)); //cs2==cs1 => true
+
+
+        //after scope prototype
+
+        System.out.println("cs1 = " + cs1); //cs1 = com.cydeo.service.CommentService@79e2c065af
+
+        System.out.println("cs2 = " + cs2); //cs2 = com.cydeo.service.CommentService@3a93b025
+
+        System.out.println("cs2==cs1 => " + (cs2==cs1)); //cs2==cs1 => false
+
+    }
+
+}
